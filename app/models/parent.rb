72888df7c97e5bab3323878dc1c53f1b2377de66
Katharina Peter <gaz@gmx.net>
2016-05-14 12:50:53 -0700

    Parent class - added students method

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,13 +1,14 @@
 class Parent
-	def initialize(person)
-		@person = person
-	end
+  def initialize(person)
+    @person = person
+  end
 
-	def children
-		contacts = StudentContact.where(person_id: @person.id, relationship_to_student: ["StudentContact.mother_role", "StudentContact.father_role"])
-		#byebug
-		contacts.map(&:student).map(&:person)
-	end
+  def students
+    contacts = StudentContact.where(person_id: @person.id, relationship_to_student: ["StudentContact.mother_role", "StudentContact.father_role"])
+    contacts.map(&:student)
+  end
 
-
+  def children
+    students.map(&:person)
+  end
 end
