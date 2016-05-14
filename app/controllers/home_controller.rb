ba117e4ecc43c439382af9594b1ed98faecd347b
Ruchi Malik <ruchi@captora.com>
2016-05-14 15:21:31 -0700

    fixed merge conflict

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,15 +4,12 @@ class HomeController < ApplicationController
   def registration_confirmation
   end
 
-<<<<<<< HEAD
    def show
   	@course = Course.find(params[:id])
   	@instructor = @course.instructor_name
   	@coordinator = @course.coordinator_name
   end
 
-=======
->>>>>>> 9234fcabe6be62322a84becae8bfffd14227e119
   def index
     @registrations = Registration.all
   end
