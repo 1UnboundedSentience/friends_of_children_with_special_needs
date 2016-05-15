02aec37c9e635cc6f153ee262dfa0838f4bcc651
jy <jackiesyu@hotmail.com>
2016-05-14 18:32:57 -0700

    Fix device controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     end
   end
 
-  devise_for :users, controllers: { registrations: "devise/registrations" }
+  devise_for :users, controllers: { registrations: "users/registrations" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
