8954fcb06bf8a8118eb5d76b1c9f8bc41829e423
bamohan <binsunmaya@gmail.com>
2016-05-30 09:14:27 -0700

    [#102] Handling of 'Next' button bug

--- app/controllers/parent/courses_controller.rb
+++ app/controllers/parent/courses_controller.rb
@@ -13,20 +13,12 @@ module Parent
     def add_basket_item
       shopping_basket.add_item_to_basket(params[:course_id])
       store_basket_in_session
-      enable_disable_next_button
       redirect_to course_selection_path
     end
 
     def remove_basket_item
       shopping_basket.remove_item_from_basket(params[:course_id])
-      enable_disable_next_button
       redirect_to course_selection_path
-    end
-
-    private
-
-    def enable_disable_next_button
-      @enable_next_button = shopping_basket.items && shopping_basket.items.count > 0
     end
   end
 end
